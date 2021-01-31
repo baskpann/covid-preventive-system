db04f96d4d07865526224302057b4368c42181b6
Baskaran Panneerselvam <baskpann@gmail.com>
2021-02-01 00:46:49 +0530

    replaced is_present with label_set

--- face-recognition/read_video.py
+++ face-recognition/read_video.py
@@ -14,17 +14,18 @@ def json_serial(obj):
     raise TypeError ("Type %s not serializable" % type(obj))
 
 now = datetime.now()
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/bas/college/projects/covid-preventive-system/face-recognition/attendancesheet-293208-852324a98bf2.json',scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('attendancesheet-293208-852324a98bf2.json', scope)
 
 gc = gspread.authorize(credentials)
 wks = gc.open('Attendance').worksheet(now.strftime("%d/%m/%Y"))
+label_set = {-1}
 
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 # Give path of where trainingData.yml is saved
-path_to_trained_data = '/home/bas/college/projects/covid-preventive-system/face-recognition/train-images/training_data.yml'
+path_to_trained_data = 'train-images/training_data.yml'
 face_recognizer.read(path_to_trained_data)
 cam = cv2.VideoCapture(0) 
 
@@ -54,15 +55,15 @@ while True:
         fr.put_text(test_img, predicted_name, x, y)
 
         # to make updation once on the sheet
-        if(name[label]["is_present"]):
+        if(label in label_set):
             # forces the next iter 
             continue
         else:
         	# this is to make user that only one entry is made.	
-            name[label]["is_present"] = True
-            wks.append_row([predicted_id, predicted_name, json.dumps(now.strftime('%H:%M:%S').strip(''), default=json_serial)])
+            label_set.add(label)
+            wks.append_row([predicted_id, predicted_name, now.strftime("%H:%M:%S")])
 
-    output_image = cv2.resize(test_img,(1000,700))
+    output_image = cv2.resize(test_img, (500,500))
 
     cv2.imshow("Face detector ", output_image)
     if cv2.waitKey(10) == ord('q'):
